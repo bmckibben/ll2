35c1ea52be5e045c349835ced816955e8459354a
bmckibben <mckibben@att.net>
2019-02-25 21:01:25 -0500

    tweeking css and nav

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,6 @@
+Time::DATE_FORMATS[:date] = "%d"
+Time::DATE_FORMATS[:month_year] = "%B %Y"
+Time::DATE_FORMATS[:day_of_week] = "%A"
+Time::DATE_FORMATS[:stardate] = "%Y.%j"
+Time::DATE_FORMATS[:standard] = "%d %B %Y"
+Time::DATE_FORMATS[:fullDT] = "%Y %m %d %H %M %S "
