13a663d93ceb76223b84383fc33f8a42e5e5279a
Ben McKibben <mckibben@att.net>
2019-07-02 10:39:36 -0400

    tinyMCE

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Literallight
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-   config.tinymce.install = :compile
-   config.autoload_paths += %W(#{config.root}/app/models/ckeditor)    
+   config.tinymce.install = :compile  
   end
 end
