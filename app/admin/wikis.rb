b0039dd07719fc9fb4b88f98e388c0bae95544c8
bmckibben <mckibben@att.net>
2020-04-19 16:31:40 -0400

    admin.wikis permitted_params

--- app/admin/wikis.rb
+++ app/admin/wikis.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Wiki do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :title, :user_id, :body, :parent, :version, :deleted, :default_sort, :last_revison
 #
 # or
 #
