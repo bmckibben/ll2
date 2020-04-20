147179d153eb698feee65ed4d93c0f93ba9bf654
bmckibben <mckibben@att.net>
2020-04-20 13:54:30 -0400

    Update FA references

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -72,7 +72,7 @@ module WikisHelper
       SELECT category_tree.*, wikis.title, wikis.default_sort 
       FROM category_tree 
             RIGHT OUTER JOIN wikis on category_tree.id = wikis.id
-      ORDER BY my_sort")
+      ORDER BY my_sort, wikis.created_at desc")
     end 
 
 
