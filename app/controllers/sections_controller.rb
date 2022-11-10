8242d49997ae1ca13d7d96b2ecedda721fc03681
Ben McKibben <github@literallight.com>
2022-11-10 10:27:07 -0500

    Update sections_controller.rb

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -12,7 +12,7 @@ class SectionsController < InheritedResources::Base
   		@note = @section.section_notes.build()
   	end
     respond_to do |format|
-      format.html {render file: "#{Rails.root}/public/404.html", layout: false}
+      format.html {render 'test.html.haml'}
       format.js {render 'edit_note_modal.js.erb'}
     end     
   end
