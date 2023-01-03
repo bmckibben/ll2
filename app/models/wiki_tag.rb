82c051a8b9c0fc6bd45e763079d7ed21198b3639
Ben McKibben <github@literallight.com>
2023-01-03 10:56:36 -0500

    Update wiki_tag.rb
    Provide parent method for wiki_tag

--- app/models/wiki_tag.rb
+++ app/models/wiki_tag.rb
@@ -1,4 +1,8 @@
 class WikiTag < ActiveRecord::Base
    belongs_to :wiki
    belongs_to :tag, :foreign_key => "tag_id", :class_name => "Wiki"
+   
+   def parent
+      Wiki.where(id: self.tag_id).pluck(:title)
+   end
 end
