98e756a15b22d7f55d8d390ffb5105b4907f42ea
Ben McKibben <github@literallight.com>
2023-01-03 14:18:24 -0500

    Update wiki.rb
    WIPO

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -4,4 +4,6 @@ class Wiki < ActiveRecord::Base
 	# gnarly many to many self join
 	has_many :wiki_tags
 	has_many :tags, through: "wiki_tags", :foreign_key => "tag_id", :class_name => "Wiki"
+	
+	accepts_nested_attributes_for :wiki_tags, :allow_destroy => true
 end
