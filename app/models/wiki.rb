05a8a7fd7789239523aabe45e212a376b6d11c5b
Ben McKibben <github@literallight.com>
2023-01-03 14:35:19 -0500

    Update wiki.rb

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -5,5 +5,7 @@ class Wiki < ActiveRecord::Base
 	has_many :wiki_tags
 	has_many :tags, through: "wiki_tags", :foreign_key => "tag_id", :class_name => "Wiki"
 	
-	accepts_nested_attributes_for :wiki_tags, :allow_destroy => true
+	#accepts_nested_attributes_for :wiki_tags, :allow_destroy => true
+	
+	attr_accessor : tags
 end
