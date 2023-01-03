d5abac63f4abe500d9993db1209632a203aeebdf
Ben McKibben <github@literallight.com>
2023-01-03 11:03:41 -0500

    Update wiki_tag.rb

--- app/models/wiki_tag.rb
+++ app/models/wiki_tag.rb
@@ -1,8 +1,5 @@
 class WikiTag < ActiveRecord::Base
    belongs_to :wiki
    belongs_to :tag, :foreign_key => "tag_id", :class_name => "Wiki"
-   
-   def parent
-      Wiki.where(id: self.tag_id).pluck(:title)
-   end
+
 end
