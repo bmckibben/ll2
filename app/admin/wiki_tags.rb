e6d6130023fe52f0650644c223591d2d5cf47968
Ben McKibben <github@literallight.com>
2023-01-06 09:30:38 -0500

    add id to permit_params

--- app/admin/wiki_tags.rb
+++ app/admin/wiki_tags.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register WikiTag do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :wiki_id, :tag_id
+permit_params :id, :wiki_id, :tag_id
 #
 # or
 #
