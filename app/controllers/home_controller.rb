52f2601f5cd9af9651aac23a0e9be40293fd3823
Ben McKibben <github@literallight.com>
2025-04-18 12:35:27 -0400

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   end  
 
   def contact
-    raise params.inspect
+
   	HomeMailer.send_contact_email(params[:email_name],params[:email_address],params[:email_message]).deliver_now
       message = 'Your message is on the way.'
       type = 'notice'
