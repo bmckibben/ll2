596e853e00340040e11a2464a84d5d2c999578c3
Ben McKibben <github@literallight.com>
2025-04-18 13:13:50 -0400

    Update home_mailer.rb

--- app/mailers/home_mailer.rb
+++ app/mailers/home_mailer.rb
@@ -1,7 +1,13 @@
 class HomeMailer < ActionMailer::Base
-def new_message(feedback, website_url)
+  def new_message(feedback, website_url)
     @feedback = feedback
     @website_url = website_url
     mail subject: 'CRG Resource Feedback', reply_to: @feedback.user.email
   end
+  def contact(name, email, message)
+    @email = email
+    @message = message
+    @name = name
+    mail subject: 'Email from Literallight', to: "ben@literallight.com", reply_to: @email, from: @email
+  end  
 end
